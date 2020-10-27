f643dddf5b68d22cf06edadf862ea2e4e7b11348
Guy Brown <guy@digitalopolis.co.uk>
2020-10-27 16:37:15 +0000

    defaulted to 'transform' as the default tenant

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const _mpConfigTenant = "serica";
+export const _mpConfigTenant = "transform";
 export const _mpConfigTaxPercent = 20;
 export const _mpConfigTaxValue = 1 + (_mpConfigTaxPercent / 100); // 20% > n*1.2
 export const _mpConfigTaxLabel = "VAT";
